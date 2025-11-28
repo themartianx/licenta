130704e983a7f4a44f795ee78d4ae9e4c6f99cb4
themartianx <xthemartian@gmail.com>
2025-11-28 17:43:49 +0200

    send and receive tcp packets

--- TCPserver.py
+++ TCPserver.py
@@ -2,7 +2,7 @@ import socket
 
 ip = "127.0.0.1"
 port = 65432
-search = True
+search = "True"
 while search == "True":
     print("connecting...")
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
@@ -10,14 +10,12 @@ while search == "True":
         sock.listen()
         conn, addr = sock.accept()
         with conn:
-            while True:
-                data = conn.recv(1024)
+            data = conn.recv(1024)
+            while data:
                 print("Received: " + str(data))
-                if not data:
-                    break
                 conn.sendall(data)
+                data = conn.recv(1024)
 
-        with open("tcpSearch.txt", "r") as f:
+        with open("switches/tcpSearch.txt", "r") as f:
             search = f.read()
-            print("search: " + search)
 
